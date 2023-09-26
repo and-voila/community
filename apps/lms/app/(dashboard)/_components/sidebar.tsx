9ea768bd002c0f68c4ed44db08cb06b706f5ec75
Sam Rizvi <srizvi@users.noreply.github.com>
2023-09-26 05:53:46 -0700

    chore: replace logo component

--- apps/lms/app/(dashboard)/_components/sidebar.tsx
+++ apps/lms/app/(dashboard)/_components/sidebar.tsx
@@ -1,11 +1,12 @@
-import { Logo } from './logo';
+import { Logo } from '@ui/index';
+
 import { SidebarRoutes } from './sidebar-routes';
 
 export const Sidebar = () => {
   return (
     <div className="flex h-full flex-col overflow-y-auto border-r bg-white shadow-sm">
       <div className="p-6">
-        <Logo />
+        <Logo fillOnHover className="h-8 md:h-10" />
       </div>
       <div className="flex w-full flex-col">
         <SidebarRoutes />
