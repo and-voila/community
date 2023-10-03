43b0404e034dbe7699e5fc8bfb50d85f17d5d6f3
Sam Rizvi <srizvi@users.noreply.github.com>
2023-10-03 02:43:07 -0700

    style(dashboard): minor style updates #130
    - cleaned up dashboard sidebar styles
      - updated quicklinks
      - removed Created by BRIL.LA
    - replaced ModeToggle dropdown with Switch
    - Closes #129
    - Closes #130
    - Closes #131

--- packages/ui/components/mode-toggle.tsx
+++ packages/ui/components/mode-toggle.tsx
@@ -1,38 +1,24 @@
 'use client';
 
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
-import { Button } from '@ui/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@ui/components/ui/dropdown-menu';
 import { useTheme } from 'next-themes';
 
+import { Switch } from './ui/switch';
+
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  const isDark = theme === 'dark';
+
+  const toggleTheme = () => {
+    setTheme(isDark ? 'light' : 'dark');
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="my-4 flex items-center">
+      <SunIcon className="mr-2 h-4 w-4" />
+      <Switch checked={isDark} onCheckedChange={toggleTheme} />
+      <MoonIcon className="ml-2 h-4 w-4" />
+    </div>
   );
 }
