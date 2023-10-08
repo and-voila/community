0c020a7a4b8cbb1f87764318d62bdfa885ba7f2c
Sam Rizvi <srizvi@users.noreply.github.com>
2023-10-08 00:02:37 -0700

    style: single post

--- apps/web/app/(main)/blog/[slug]/page.tsx
+++ apps/web/app/(main)/blog/[slug]/page.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 import { PostMdx } from '../_components/mdx/post-mdx';
-import Newsletter from '../_components/newsletter';
 import PostDate from '../_components/post-date';
 import RelatedPosts02 from '../_components/related-posts-02';
 
@@ -45,7 +44,7 @@ export default async function SinglePost({
       <section className="relative">
         {/* Background image */}
         {post.image && (
-          <div className="absolute inset-0 box-content h-40 pt-16">
+          <div className="absolute inset-0 box-content h-96 pt-16">
             <Image
               className="absolute inset-0 h-full w-full object-cover opacity-25"
               src={post.image}
@@ -55,43 +54,29 @@ export default async function SinglePost({
               alt={post.title}
             />
             <div
-              className="absolute inset-0 bg-gradient-to-t from-white dark:from-gray-900"
+              className="absolute inset-0 bg-gradient-to-t from-background"
               aria-hidden="true"
             />
           </div>
         )}
-
         <div className="relative mx-auto max-w-6xl px-4 sm:px-6">
           <div className="pb-12 pt-32 md:pb-20 md:pt-40">
+            <div className="text-center md:text-left">
+              <h1 className="py-6 font-display text-7xl md:text-9xl">
+                {post.title}
+              </h1>
+            </div>
             <div className="mx-auto max-w-3xl">
               <article>
                 {/* Article header */}
                 <header className="mb-8">
                   {/* Title and excerpt */}
-                  <div className="text-center md:text-left">
-                    <h1
-                      className="h1 font-red-hat-display mb-4"
-                      data-aos="fade-down"
-                    >
-                      {post.title}
-                    </h1>
-                    <p
-                      className="text-xl text-gray-600 dark:text-gray-400"
-                      data-aos="fade-down"
-                      data-aos-delay="150"
-                    >
-                      {post.summary}
-                    </p>
-                  </div>
+
                   {/* Article meta */}
                   <div className="mt-5 md:flex md:items-center md:justify-between">
                     {/* Author meta */}
-                    <div
-                      className="flex items-center justify-center"
-                      data-aos="fade-down"
-                      data-aos-delay="300"
-                    >
-                      <a href="#0">
+                    <div className="flex items-center justify-center">
+                      <div className="rounded-full bg-background">
                         <Image
                           className="mr-3 shrink-0 rounded-full"
                           src={post.authorImg}
@@ -99,17 +84,14 @@ export default async function SinglePost({
                           height={32}
                           alt={post.author}
                         />
-                      </a>
-                      <div>
+                      </div>
+                      <div className="flex items-center">
                         <span className="text-gray-600 dark:text-gray-400">
                           By{' '}
                         </span>
-                        <a
-                          className="font-medium text-gray-800 hover:underline dark:text-gray-300"
-                          href="#0"
-                        >
+                        <div className="font-medium text-gray-800 hover:underline dark:text-gray-300">
                           {post.author}
-                        </a>
+                        </div>
                         <span className="text-gray-600 dark:text-gray-400">
                           {' '}
                           · <PostDate dateString={post.publishedAt} />
@@ -118,21 +100,15 @@ export default async function SinglePost({
                     </div>
                   </div>
                 </header>
-                <hr
-                  className="mb-8 h-px w-5 border-0 bg-gray-400 pt-px dark:bg-gray-500"
-                  data-aos="fade-down"
-                  data-aos-delay="450"
-                />
+                <hr className="mb-8 h-px w-5 border-0 bg-gray-400 pt-px dark:bg-gray-500" />
 
                 {/* Article content */}
-                <div className="mb-8" data-aos="fade-up" data-aos-delay="450">
+                <div className="mb-8">
                   <PostMdx code={post.body.code} />
                 </div>
 
                 {/* Article footer */}
-                <footer>
-                  <Newsletter />
-                </footer>
+                <footer />
               </article>
             </div>
           </div>
