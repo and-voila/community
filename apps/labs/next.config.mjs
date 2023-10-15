34ac319494bece7ba6f02df0dd4c8155e233037a
Sam Rizvi <srizvi@users.noreply.github.com>
2023-10-14 20:26:25 -0700

    feat: shared next.config #190
    - add transpilePackages option to include 'ui' package for transpilation
    - enable serverActions in experimental options
    
    Closes #190

--- apps/labs/next.config.mjs
+++ apps/labs/next.config.mjs
@@ -1,16 +1,17 @@
 import { withContentlayer } from "next-contentlayer"
-
 import "./env.mjs"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['ui'],
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["@prisma/client"],
+    serverActions: true,
   },
 }
 
