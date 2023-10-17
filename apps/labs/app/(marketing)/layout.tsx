bb8ec60a75d1b9062e9d51bf9afc9f8e64db1b5e
Sam Rizvi <srizvi@users.noreply.github.com>
2023-10-17 03:22:54 -0700

    feat: add sign up button to layout #197
    Closes #197

--- apps/labs/app/(marketing)/layout.tsx
+++ apps/labs/app/(marketing)/layout.tsx
@@ -2,9 +2,11 @@ import Link from 'next/link';
 import { cn } from '@ui/lib/utils';
 
 import { marketingConfig } from '@/config/marketing';
+import { getCurrentUser } from '@/lib/session';
 import { buttonVariants } from '@/components/ui/button';
 import { MainNav } from '@/components/main-nav';
 import { SiteFooter } from '@/components/site-footer';
+import { UserAccountNav } from '@/components/user-account-nav';
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -13,22 +15,42 @@ interface MarketingLayoutProps {
 export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
+  const user = await getCurrentUser();
+
   return (
     <div className="flex min-h-screen flex-col">
       <header className="container z-40 bg-background">
         <div className="flex h-20 items-center justify-between py-6">
           <MainNav items={marketingConfig.mainNav} />
-          <nav>
-            <Link
-              href="/login"
-              className={cn(
-                buttonVariants({ variant: 'secondary', size: 'sm' }),
-                'px-4',
-              )}
-            >
-              Login
-            </Link>
-          </nav>
+          {user ? (
+            <UserAccountNav
+              user={{
+                name: user.name,
+                image: user.image,
+                email: user.email,
+              }}
+            />
+          ) : (
+            <nav>
+              <Link
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: 'ghost' }),
+                  'px-4 mr-4',
+                )}
+                aria-label="Navigate to Login page"
+              >
+                Login
+              </Link>
+              <Link
+                href="/register"
+                className={cn(buttonVariants({ variant: 'custom' }), 'px-4')}
+                aria-label="Navigate to sign up page"
+              >
+                Sign Up
+              </Link>
+            </nav>
+          )}
         </div>
       </header>
       <main className="flex-1">{children}</main>
