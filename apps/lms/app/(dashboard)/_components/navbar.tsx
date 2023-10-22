bbcdfd5c06fda08b1bc5ce0d5be32091a839694b
Sam Rizvi <srizvi@users.noreply.github.com>
2023-10-22 02:14:18 -0700

    chore: address remnants causing build issues

--- apps/lms/app/(dashboard)/_components/navbar.tsx
+++ apps/lms/app/(dashboard)/_components/navbar.tsx
@@ -1,12 +1,19 @@
+import { getApiLimitCount } from '@/lib/api-limit';
+import { checkSubscription } from '@/lib/subscription';
 import { NavbarRoutes } from '@/components/navbar-routes';
 
 import { MobileSidebar } from './mobile-sidebar';
 
-export const Navbar = () => {
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount();
+  const isPro = await checkSubscription();
+
   return (
     <div className="flex h-full items-center border-b bg-[#dcdfe5] p-4 shadow-sm dark:bg-[#16161a]">
-      <MobileSidebar />
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       <NavbarRoutes />
     </div>
   );
 };
+
+export default Navbar;
