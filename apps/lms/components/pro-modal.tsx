a13f546dc047a343f4389b3be5ba0c2725edcb3d
Sam Rizvi <srizvi@users.noreply.github.com>
2023-10-23 04:42:30 -0700

    style: update pro modal
    - cleaned up styles on pro modal
    - added early access coupon to checkout
    - centralized more icons

--- apps/lms/components/pro-modal.tsx
+++ apps/lms/components/pro-modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Link from 'next/link';
 import axios from 'axios';
 import { useProModal } from 'hooks/use-pro-modal';
 import toast from 'react-hot-toast';
@@ -8,51 +9,46 @@ import {
   Badge,
   Button,
   Card,
-  CheckIcon,
-  CodeIcon,
   Dialog,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  ImageIcon,
-  MagicWandIcon,
-  Pencil1Icon,
-  SpeakerLoudIcon,
-  VideoIcon,
 } from 'ui';
 
-const tools = [
+import { Icons } from './icons';
+
+const features = [
   {
-    label: 'Write',
-    icon: Pencil1Icon,
-    cta: 'Compose',
-    description: 'Draft amazing content in your personal style.',
+    label: 'Community',
+    icon: Icons.heart,
+    description: 'Full access to Discord community.',
   },
   {
-    label: 'Image',
-    icon: ImageIcon,
-    cta: 'Create',
-    description: 'Create images like a boss assisted by AI.',
+    label: 'Labs',
+    icon: Icons.magic,
+    description: 'Experience a complete marketing ecosystem.',
   },
   {
-    label: 'Video',
-    icon: VideoIcon,
-    cta: 'Produce',
-    description: 'Produce videos and go viral in a few clicks.',
+    label: 'Playbooks',
+    icon: Icons.image,
+    description: 'Full access to Playbook library.',
   },
   {
-    label: 'Music',
-    icon: SpeakerLoudIcon,
-    cta: 'Jam',
-    description: 'Crank out jingles and jams just like Tyler.',
+    label: 'AI Tools',
+    icon: Icons.briefcase,
+    description: 'Cool AI tools to help your workflow.',
   },
   {
-    label: 'Code',
-    icon: CodeIcon,
-    cta: 'Hack',
-    description: 'A coder on demand to build anything.',
+    label: 'Workshops',
+    icon: Icons.desktop,
+    description: 'Exclusive weekly sessions by experts.',
+  },
+  {
+    label: 'Results',
+    icon: Icons.rocket,
+    description: 'Put it all together, and voila! ROI on fire.',
   },
 ];
 
@@ -78,29 +74,35 @@ export const ProModal = () => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="flex flex-col items-center justify-center gap-y-4 pb-2">
-            <div className="flex items-center gap-x-2 py-1 font-bold">
-              Subscribe to Premium
-              <Badge variant="default" className="py-1 text-sm uppercase">
-                Pro
+            <div className="flex items-center gap-x-2 py-1 font-display text-2xl">
+              Become a paid member
+              <Badge variant="custom" className="py-1 text-sm uppercase">
+                The Best Plan
               </Badge>
             </div>
           </DialogTitle>
-          <DialogDescription className="space-y-2 pt-2 text-center font-medium text-stone-900">
-            {tools.map((tool) => (
+          <DialogDescription className="space-y-2 pt-2 text-center font-medium text-muted-foreground">
+            <p className="text-muted-foreground text-sm lg:text-base mb-6">
+              Skyrocket your marketing performance for only $50 per month with
+              complete access to the Community and Labs.
+            </p>
+            {features.map((feature) => (
               <Card
-                key={tool.label}
+                key={feature.label}
                 className="flex items-center justify-between border p-3"
               >
                 <div className="flex items-center gap-x-4">
                   <div className="w-fit rounded-md p-2 text-muted-foreground">
-                    <tool.icon className="h-6 w-6" />
+                    <feature.icon className="h-4 w-4" />
                   </div>
-                  <div className="text-base font-bold">{tool.label}</div>
-                  <div className="text-sm text-muted-foreground">
-                    {tool.description}
+                  <div className="text-base font-semi-bold">
+                    {feature.label}
+                  </div>
+                  <div className="text-sm text-muted-foreground text-left">
+                    {feature.description}
                   </div>
                 </div>
-                <CheckIcon className="h-5 w-5 text-primary" />
+                <Icons.check className="h-5 w-5 text-brand" />
               </Card>
             ))}
           </DialogDescription>
@@ -114,12 +116,31 @@ export const ProModal = () => {
             className="mt-2 w-full"
           >
             Upgrade
-            <MagicWandIcon className="ml-2 h-4 w-4" />
+            <Icons.magic className="ml-2 h-4 w-4" />
           </Button>
         </DialogFooter>
-        <p className="text-center text-sm text-muted-foreground">
-          100% delight money-back guarantee. Cancel anytime.
+        <p className="text-center text-xs text-muted-foreground max-w-sm mx-auto">
+          100% delight money-back guarantee. Cancel anytime. <br /> Read our{' '}
+          <Link
+            href="https://andvoila.gg/terms"
+            target="_blank"
+            className="underline"
+            aria-label="Navigate to external link to read And Voila Terms of Service"
+          >
+            Terms of Service
+          </Link>{' '}
+          and{' '}
+          <Link
+            href="https://andvoila.gg/privacy"
+            target="_blank"
+            aria-label="Navigate to external link to read And Voila Privacy Policy"
+            className="underline"
+          >
+            Privacy Policy
+          </Link>
+          .
         </p>
+        <div className="text-center text-xs text-muted-foreground" />
       </DialogContent>
     </Dialog>
   );
--- packages/ui/components/ui/badge.tsx
+++ packages/ui/components/ui/badge.tsx
@@ -14,6 +14,8 @@ const badgeVariants = cva(
         destructive:
           'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
         outline: 'text-foreground',
+        custom:
+          'border-alternate bg-transparent text-alternate shadow pointer-events-none',
       },
     },
     defaultVariants: {
