74d6ad88184172e99c5d476841d942502999b9c5
Sam Rizvi <srizvi@users.noreply.github.com>
2023-10-22 19:08:21 -0700

    feat: optimize prisma client with accelerate
    - updated prisma client to [instantiate singleton first](https://github.com/prisma/accelerate-nextjs-starter/blob/main/lib/db.ts)

--- apps/lms/lib/db.ts
+++ apps/lms/lib/db.ts
@@ -1,12 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate());
+};
 
-export const db = (globalThis.prisma ||
-  new PrismaClient().$extends(withAccelerate())) as PrismaClient;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export const db = prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
