b6e9e93db882956d6466ba89d7e15a055c53d87c
Sam Rizvi <srizvi@users.noreply.github.com>
2023-10-30 22:10:58 -0700

    feat: add app icons
    - added icons
    - added manifest
    - added browserconfig
    
    Closes #270

--- apps/lms/app/config/site.ts
+++ apps/lms/app/config/site.ts
@@ -5,10 +5,10 @@ export const siteConfig: SiteConfig = {
   name: 'And Voila',
   description:
     'An open source application built using the new router, server components and everything new in Next.js 13.',
-  url: 'https://labs.andvoila.app',
+  url: 'https://labs.andvoila.gg',
   ogImage: '/open-graph.gif',
   links: {
     twitter: 'https://twitter.com/rebekahradice',
-    github: 'https://github.com/and-voila/community/tree/main/apps/labs',
+    github: 'https://github.com/and-voila/community/',
   },
 };
