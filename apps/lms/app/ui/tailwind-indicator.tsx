a115f998d41845d07214fdcc96d075730a4c802f
Sam Rizvi <srizvi@users.noreply.github.com>
2023-11-03 04:03:05 -0700

    chore: clean up tailwind indicator display

--- apps/lms/app/ui/tailwind-indicator.tsx
+++ apps/lms/app/ui/tailwind-indicator.tsx
@@ -1,5 +1,5 @@
 export function TailwindIndicator() {
-  if (process.env.NODE_ENV === 'production') return null;
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') return null;
 
   return (
     <div className="fixed bottom-6 right-56 z-50 flex h-10 w-10 items-center justify-center rounded-full bg-brand p-3 font-mono text-xs text-white">
